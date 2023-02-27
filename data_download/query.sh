315102169cfd3a58dd2365448426381a1b8b8299
Caleb Lindgren <calebmlindgren@gmail.com>
2023-02-27 08:05:50 -0700

    Remove extra query file

--- data_download/query.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-esearch -db sra -query 'monkeypox[All Fields] AND "Monkeypox virus"[orgn] AND (cluster_public[prop] AND "biomol dna"[Properties] AND "strategy wgs"[Properties] AND "library layout paired"[Properties] AND "platform illumina"[Properties] AND "strategy wgs"[Properties] OR "strategy wga"[Properties] OR "strategy wcs"[Properties] OR "strategy clone"[Properties] OR "strategy finishing"[Properties] OR "strategy validation"[Properties] AND "filetype fastq"[Properties])' | \
-efetch -format docsum
