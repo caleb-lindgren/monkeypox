25be6560cc34936421d8e37578907d2ab5d4af26
Caleb Lindgren <calebmlindgren@gmail.com>
2023-03-04 18:58:01 -0700

    Update job array script for SRA extraction

--- data_download/extract_sra_job_array.sh
+++ data_download/extract_sra_job_array.sh
@@ -3,14 +3,18 @@
 #SBATCH --time=00:10:00   # walltime
 #SBATCH --ntasks=1   # number of processor cores (i.e. tasks)
 #SBATCH --nodes=1   # number of nodes
-#SBATCH --mem-per-cpu=2000M   # memory per CPU core
+#SBATCH --mem-per-cpu=1G   # memory per CPU core
 #SBATCH --mail-user=calebmlindgren@gmail.com   # email address
 #SBATCH --mail-type=BEGIN
 #SBATCH --mail-type=END
 #SBATCH --mail-type=FAIL
-#SBATCH --output=output/slrm_%a.out 
-#SBATCH --array=0-742
+#SBATCH --output=output/slurm_%a.out 
+#SBATCH --array=0-2
 
-ACC=$(ls $1 | tr " " "\n" | head -$(($SLURM_ARRAY_TASK_ID + 1)) | tail -1)
-mkdir $2/$ACC
-fasterq-dump $1/$ACC -O $2/$ACC
+INPUT_DIR=../../../../fsl_groups/grp_bio465_mpxv/compute/small_in
+OUTPUT_DIR=../../../../fsl_groups/grp_bio465_mpxv/compute/small_out
+
+ACC=$(ls $INPUT_DIR | tr " " "\n" | head -$(($SLURM_ARRAY_TASK_ID + 1)) | tail -1)
+mkdir $OUTPUT_DIR/$ACC
+
+fasterq-dump $INPUT_DIR/$ACC -O $OUTPUT_DIR/$ACC
