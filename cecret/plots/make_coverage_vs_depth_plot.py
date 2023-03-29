5d3731cbe62d70d71d987b0413574d1d51de55cc
Caleb Lindgren <calebmlindgren@gmail.com>
2023-03-28 21:12:03 -0600

    Finish first draft of depth vs. coverage chart

--- cecret/plots/make_coverage_vs_depth_plot.py
+++ cecret/plots/make_coverage_vs_depth_plot.py
@@ -4,8 +4,11 @@ import pandas as pd
 all_summary = pd.read_csv("all_summary.tsv", sep="\t")
 
 chart = alt.Chart(all_summary).mark_circle().encode(
-    x="coverage",
+    x=alt.X(
+        "coverage",
+        scale=alt.Scale(zero=False),
+    ),
     y="depth",
 )
 
-chart.save("coverage_vs_depth_plot.png")
+chart.save("coverage_vs_depth_plot.png", scale_factor=4)
