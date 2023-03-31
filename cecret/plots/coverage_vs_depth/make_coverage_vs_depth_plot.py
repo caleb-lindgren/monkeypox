aa6ba86133ef7017d30563e08ad4c0e454c6e3db
Caleb Lindgren <calebmlindgren@gmail.com>
2023-03-30 22:32:17 -0600

    Add better labels to figure 3

--- cecret/plots/coverage_vs_depth/make_coverage_vs_depth_plot.py
+++ cecret/plots/coverage_vs_depth/make_coverage_vs_depth_plot.py
@@ -6,9 +6,13 @@ all_summary = pd.read_csv("all_summary.tsv", sep="\t")
 chart = alt.Chart(all_summary).mark_circle().encode(
     x=alt.X(
         "coverage",
+        title="MPXV genome coverage (%)",
         scale=alt.Scale(zero=False),
     ),
-    y="depth",
+    y=alt.Y(
+        "depth",
+        title="Depth of coverage (mean number of reads)",
+    ),
 )
 
 chart.save("coverage_vs_depth_plot.png", scale_factor=4)
--- cecret/plots/fastqc/make_fastqc_table.py
+++ cecret/plots/fastqc/make_fastqc_table.py
@@ -20,23 +20,6 @@ empty_count = 0
 
 for sample_dir in sample_dirs:
     all_count += 1
-    summary_file = os.path.join(sample_dir, "cecret_results.csv")
-    if os.path.isfile(summary_file):
-        summary_df = pd.read_csv(summary_file)
-        samples.append(summary_df.loc[0, "sample_id"])
-        depths.append(summary_df.loc[0, "depth_after_trimming"])
-        coverages.append(summary_df.loc[0, "1X_coverage_after_trimming"])
-        perc_kepts.append(summary_df.loc[0, "seqyclean_Perc_Kept"])
+    if os.path.isfile():
     else:
         empty_count += 1
-
-print(f"{empty_count} of {all_count} input directories were empty.")
-
-all_summary = pd.DataFrame({
-    "accession": samples,
-    "depth": depths,
-    "coverage": coverages,
-    "perc_kept": perc_kepts,
-})
-
-all_summary.to_csv("all_summary.tsv", sep="\t", index=False)
